472bf24ab24bcb7eddc5262f292a56fd850a3c80
Krupalbhoi <162288340+Krupalbhoi@users.noreply.github.com>
2025-08-08 20:08:53 +0530

    Update FirebaseConfig.jsx

--- src/firebase/FirebaseConfig.jsx
+++ src/firebase/FirebaseConfig.jsx
@@ -1,27 +1,4 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getFirestore } from 'firebase/firestore';
-// import { getAuth } from 'firebase/auth';
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDBHTUtOeuCON7AEF3UPPxU1sHsnFfBv-M",
-//     authDomain: "ecommerce-for.firebaseapp.com",
-//     projectId: "ecommerce-for",
-//     storageBucket: "ecommerce-for.appspot.com",
-//     messagingSenderId: "609664686595",
-//     appId: "1:609664686595:web:302bf25cc8efa64a4e4c36"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-
-// const fireDB = getFirestore(app);
-// const auth = getAuth(app);
-
-// export { fireDB, auth }
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
 import { getFirestore } from "firebase/firestore";
@@ -32,7 +9,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBftwRBJOtUQDSBI2R8bev49ylA_dvKHaA",
+  apiKey: "Add your api key",
   authDomain: "oams-97176.firebaseapp.com",
   projectId: "oams-97176",
   storageBucket: "oams-97176.appspot.com",
